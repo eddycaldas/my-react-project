4a371f3051064d2dd965ddd62e56f707046895f2
Eddy Caldas <eddycaldas@live.com>
2017-12-12 20:29:27 -0700

    with Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+//components
+import Header from './components/headerComponent/Header';
+import Footer from './components/footerComponent/Footer';
+import Homepage from './components/pages/Homepage';
+import Products from './components/pages/Products';
+
+//includes
+import './Assets/css/default.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        test
-      </div>
+      <Router>
+        <div className="App">
+
+          <Header />
+
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/Products' component={Products} />
+
+          <Footer />
+
+        </div>
+      </Router>
     );
   }
 }
